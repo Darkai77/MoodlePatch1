b2da815ace5c956f47d4be77c3dc2f8b5d7a4213
Darkai77 <34649704+Darkai77@users.noreply.github.com>
2018-01-08 14:03:30 +0100

    MDL-61149 add support

--- enrol/database/lang/en/enrol_database1.php
+++ enrol/database/lang/en/enrol_database1.php
@@ -1,3 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_database', language 'en'.
+ *
+ * @package   enrol_database
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['autocreatecategory'] = 'Create course categories';
 $string['autocreatecategory_desc'] = 'If courses are automatically created and they belong to a category that does not exist in Moodle yet, those categories can be created automatically';
 $string['categoryseparator'] = 'Category separator';
@@ -60,3 +84,4 @@ $string['settingsheadernewcourses'] = 'Creation of new courses';
 $string['remoteuserfield_desc'] = 'The name of the field in the remote table that we are using to match entries in the user table.';
 $string['templatecourse'] = 'New course template';
 $string['templatecourse_desc'] = 'Optional: auto-created courses can copy their settings from a template course. Type here the shortname of the template course. Used when no new course template field is specified or the template course is not found';
+ of the template course. Used when no new course template field is specified or the template course is not found';
